baeca8ba463f4568353e2ffa49179c802f74ece0
Mathews <baiomathews@gmail.com>
2025-04-07 21:29:06 -0300

    primeira parte do footer

--- cscl-site/src/components/footer/Footer.jsx
+++ cscl-site/src/components/footer/Footer.jsx
@@ -1,10 +1,51 @@
-import { mainColor } from "../../config/Colors";
+import { mainColor, whiteColor } from "../../config/Colors";
 
 export default function Footer() {
   return (
     <div
-      className="w-full h-[33vh]"
-      style={{ backgroundColor: mainColor }}
-    ></div>
+      className="flex flex-col gap-[1rem] justify-center items-center w-full h-[33vh] text-[0.9rem]"
+      style={{ backgroundColor: mainColor, color: whiteColor }}
+    >
+      <div className="flex flex-row w-[85vw] justify-between items-center border-b-2 border-black  p-[0.7rem]">
+        <img
+          src="assets/icons/logo.png"
+          alt="logo da Casa São Camillo de Lelis"
+          className="w-[8rem]"
+        />
+        <p>
+          Nosso compromisso é oferecer acolhimento integral às pessoas em
+          situação de rua, promovendo dignidade e cidadania por meio de ações.
+        </p>
+        <nav className="flex flex-col gap-[0.4rem]">
+          <a
+            className="hover:underline decoration-[#211F1F] underline-offset-4 cursor-pointer"
+            href="#about"
+          >
+            Sobre Nós
+          </a>
+          <a
+            className="hover:underline decoration-[#211F1F] underline-offset-4 cursor-pointer"
+            href="#cards"
+          >
+            Programas
+          </a>
+          <a
+            className="hover:underline decoration-[#211F1F] underline-offset-4 cursor-pointer"
+            href="#stories"
+          >
+            Nossas Histórias
+          </a>
+          <a
+            className="hover:underline decoration-[#211F1F] underline-offset-4 cursor-pointer"
+            href="#donations"
+          >
+            Doar Agora
+          </a>
+        </nav>
+      </div>
+      <div>
+        <p>teste</p>
+      </div>
+    </div>
   );
 }
