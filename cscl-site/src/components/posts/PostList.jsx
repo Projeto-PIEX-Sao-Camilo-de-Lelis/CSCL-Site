0c72e8db4bee1714db58d8ac4dbc46883792e9d9
RenanGuedes95 <renan.brega@viannasempre.com.br>
2025-04-12 22:06:01 -0300

    Criação do componente Field.js para ser usado na tela de login. Inclusão do tamanho do arredondamento da borda do botão na interface da lista de postagem.

--- cscl-site/src/components/posts/PostList.jsx
+++ cscl-site/src/components/posts/PostList.jsx
@@ -70,7 +70,7 @@ const PostList = () => {
                 <button
                     onClick={handlePreviousPage}
                     disabled={currentPage === 1}
-                    className={`px-4 py-2 rounded bg-main text-white ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700"
+                    className={`px-4 py-2 rounded-2xl bg-main text-white ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700"
                         }`}
                 >
                     Anterior
@@ -81,7 +81,7 @@ const PostList = () => {
                 <button
                     onClick={handleNextPage}
                     disabled={currentPage === postsData.totalPages}
-                    className={`px-4 py-2 rounded bg-main text-white ${currentPage === postsData.totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700"
+                    className={`px-4 py-2 rounded-2xl bg-main text-white ${currentPage === postsData.totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-red-700"
                         }`}
                 >
                     Próxima
