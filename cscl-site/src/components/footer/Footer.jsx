54d2ca319d212cf820acbcc93ec7c07b97f4fb4a
Mathews <baiomathews@gmail.com>
2025-06-06 19:47:38 -0300

    Novo footer e botões linkando as mídias sociais da ONG

--- cscl-site/src/components/footer/Footer.jsx
+++ cscl-site/src/components/footer/Footer.jsx
@@ -1,45 +1,24 @@
+import SocialMedia from "./SocialMedia";
+
 export default function Footer() {
   return (
     <div className="flex flex-col gap-[1rem] justify-center items-center w-full min-h-[25vh] text-[0.9rem] bg-main text-whiteColor">
-      <div className="flex flex-row w-[85vw] justify-between items-center border-b-2 border-secondary  p-[0.7rem]">
-        <img
-          src="/assets/icons/logo.png"
-          alt="logo da Casa São Camillo de Lelis"
-          className="w-[8rem]"
-        />
+      <div className="flex flex-col w-[85vw] justify-center items-center border-b-2 border-secondary  p-[0.7rem]">
+        <SocialMedia />
         <p className=" hidden sm:block">
           Nosso compromisso é oferecer acolhimento integral às pessoas em
           situação de rua, promovendo dignidade e cidadania por meio de ações.
         </p>
-        <nav className="flex flex-col gap-[0.4rem]">
-          <a
-            className="hover:underline decoration-secondary underline-offset-4 cursor-pointer"
-            href="#about"
-          >
-            Sobre Nós
-          </a>
-          <a
-            className="hover:underline decoration-secondary underline-offset-4 cursor-pointer"
-            href="#cards"
-          >
-            Programas
-          </a>
-          <a
-            className="hover:underline decoration-secondary underline-offset-4 cursor-pointer"
-            href="#stories"
-          >
-            Nossas Histórias
-          </a>
-          <a
-            className="hover:underline decoration-secondary underline-offset-4 cursor-pointer"
-            href="#donations"
-          >
-            Doar Agora
-          </a>
-        </nav>
       </div>
       <div>
         <p>2024@Desenvolvido por alunos do Instituto Vianna Júnior</p>
+      </div>
+      <div className="w-full min-h-[20vh] bg-secondary flex flex-row justify-center items-center">
+        <img
+          src="/assets/icons/logo3.png"
+          alt="logo da Casa São Camillo de Lelis"
+          className="w-[8rem]"
+        />
       </div>
     </div>
   );
